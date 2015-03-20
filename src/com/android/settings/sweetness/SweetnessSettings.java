9fb2ae9eac5e0227db2d5cff6d300bc974812a60
Don <cjkacz@gmail.com>
2015-03-20 11:05:08 -0400

    Fix-Statusbar clock and date customizations

---
+++ src/com/android/settings/sweetness/SweetnessSettings.java
@@ -0,0 +1,17 @@
+
+package com.android.settings.sweetness;
+
+import android.os.Bundle;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class SweetnessSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.sweetness_settings);
+    }
+}
