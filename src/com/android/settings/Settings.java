3c5d9ce0ef16007096990bd5d8b0720589232f95
hedwig34 <hedwig34@gmail.com>
2015-03-27 09:14:14 -0400

    Tentacles: Add Tentacles
    Change-Id: Ic3fcc4871ca927c84b29820ea2648c19b0182941
    
    Conflicts:
    	AndroidManifest.xml
    	proguard.flags
    	res/xml/device_info_settings.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java
    	src/com/android/settings/search/Ranking.java
    	src/com/android/settings/search/SearchIndexableResources.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -101,5 +101,11 @@ public class Settings extends SettingsActivity {
 
     public static class TopLevelSettings extends SettingsActivity { /* empty */ }
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class BlacklistSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class ProfilesSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class OctosSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class SoundSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class LockScreenSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class LiveDisplayActivity extends SettingsActivity { /* empty */ }
 }
 
